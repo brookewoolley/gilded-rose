94129c4a2fe6d5cf6a822f94c934ea327736df89
Brooke Woolley <brooke.woolley@gmail.com>
2019-03-16 15:30:32 +0000

    Tests for quality and sell_in values decreasing by -1

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,7 +1,6 @@
 require 'gilded_rose'
 
 describe GildedRose do
-
   describe "#update_quality" do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
@@ -9,12 +8,16 @@ describe GildedRose do
       expect(items[0].name).to eq "foo"
     end
 
-    it 'does not have a quality status of over 50' do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
+    it "decreases the quality value at the end of each day " do
+      items = [Item.new("foo", 20, 50)]
+      expect{ GildedRose.new(items).update_quality() }.to change{ items[0].quality }.by(-1)
     end
 
-    # - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+    it "decreases the sell_in value at the end of each day " do
+      items = [Item.new("foo", 20, 50)]
+      expect{ GildedRose.new(items).update_quality() }.to change{ items[0].sell_in }.by(-1)
+    end
+
     it 'does not update the quality of Sulfuras' do
       item = [Item.new("Sulfuras, Hand of Ragnaros", 0, 0)]
       GildedRose.new(item).update_quality()
@@ -26,23 +29,5 @@ describe GildedRose do
       GildedRose.new(item).update_quality()
       expect(item[0].sell_in).to eq(0)
     end
-
   end
-
 end
-
-#
-# - All items have a SellIn value which denotes the number of days we have to sell the item
-# - All items have a Quality value which denotes how valuable the item is
-# - At the end of each day our system lowers both values for every item
-#
-# Pretty simple, right? Well this is where it gets interesting:
-#
-# - Once the sell by date has passed, Quality degrades twice as fast
-# - The Quality of an item is never negative
-# - "Aged Brie" actually increases in Quality the older it gets
-# - The Quality of an item is never more than 50
-# - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-# - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
-# Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
-# Quality drops to 0 after the concert
