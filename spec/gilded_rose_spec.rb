4b5fb2c09cd075fb368163b357f5dd077de10826
Brooke Woolley <brooke.woolley@gmail.com>
2019-03-16 15:38:52 +0000

    test: decreases the quality value by 2 if the sell by date has passed

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -13,21 +13,28 @@ describe GildedRose do
       expect{ GildedRose.new(items).update_quality() }.to change{ items[0].quality }.by(-1)
     end
 
+    it 'decreases the quality value by 2 if the sell by date has passed' do
+      items = [Item.new("foo", 0, 50)]
+      expect{ GildedRose.new(items).update_quality() }.to change{ items[0].quality }.by(-2)
+    end
+
+
     it "decreases the sell_in value at the end of each day " do
       items = [Item.new("foo", 20, 50)]
       expect{ GildedRose.new(items).update_quality() }.to change{ items[0].sell_in }.by(-1)
     end
 
     it 'does not update the quality of Sulfuras' do
-      item = [Item.new("Sulfuras, Hand of Ragnaros", 0, 0)]
-      GildedRose.new(item).update_quality()
-      expect(item[0].quality).to eq(0)
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(0)
     end
 
     it 'does not update the sell_in value of Sulfuras' do
-      item = [Item.new("Sulfuras, Hand of Ragnaros", 0, 0)]
-      GildedRose.new(item).update_quality()
-      expect(item[0].sell_in).to eq(0)
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq(0)
     end
+
   end
 end
